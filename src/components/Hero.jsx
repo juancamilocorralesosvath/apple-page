a46bbd9cd92894fb89ebbe61df4919a29776789a
juancamilocorralesosvath <juancamilocorra@gmail.com>
2025-12-13 22:03:22 -0500

    feat: hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from 'react'
+
+const Hero = () => {
+    const videoRef = useRef();
+
+    useEffect(() => {
+      if (videoRef.current) videoRef.current.playbackRate =2
+      
+    }, [])
+    
+
+  return (
+    <section id='hero'>
+        <div>
+            <h1>Macbook Pro</h1>
+            <img src="title.png" alt="macbook title" />
+            <video src="/videos/hero.mp4" autoPlay muted playsInline ref={videoRef}></video>
+            <button>Buy</button>
+            <p>From $1599 or $133/mo for 12 months</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
