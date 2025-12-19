a240809ffd57ff1dfb93ad0bd43c9059c5a66895
juancamilocorralesosvath <juancamilocorra@gmail.com>
2025-12-18 23:07:33 -0500

    error: there is an error not showing the page

--- src/components/three/ModelSwitcher.jsx
+++ src/components/three/ModelSwitcher.jsx
@@ -1,19 +1,26 @@
 // rendering both mascbooks, animating transitions, presentation controls
 import { PresentationControls } from '@react-three/drei'
 import React, { useRef } from 'react'
+import MacbookModel16 from '../models/Macbook-16'
+
 
 const ModelSwitcher = ({scale, isMobile}) => {
     const smallMacbookRef = useRef()
     const largeMacbookRef = useRef()
 
-    const showLargeMacbook = scale === 0.08 || scale === 0.05
+    //const showLargeMacbook = scale === 0.08 || scale === 0.05
   return (
     <>
         <PresentationControls>
             <group ref={largeMacbookRef}>
-                <MacbookModel16
+                <MacbookModel16 scale={isMobile ? 0.05 : 0.08} />
             </group>
         </PresentationControls>
+        {/* <PresentationControls>
+            <group ref={smallMacbookRef}>
+                <MacbookModel14 scale={isMobile ? 0.03 : 0.06} />
+            </group>
+        </PresentationControls> */}
     </>
   )
 }
