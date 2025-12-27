743931b96ed7a65bded3ae7642e6d0bf0fa2ada5
juancamilocorralesosvath <juancamilocorra@gmail.com>
2025-12-27 08:51:50 -0500

    fix: fixed error. bad import on productviewer

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,7 +3,7 @@ import useMacbookStore from '../store'
 import clsx from 'clsx'
 import { Canvas } from '@react-three/fiber'
 import { Box, OrbitControls } from '@react-three/drei'
-import { MacbookModel14 } from './models/Macbook-14'
+
 import StudioLights from './three/StudioLights'
 import ModelSwitcher from './three/ModelSwitcher'
 import { useMediaQuery } from 'react-responsive'
