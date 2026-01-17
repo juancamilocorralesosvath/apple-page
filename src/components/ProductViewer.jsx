fd8ba619f0234cc03466f0d83db44d61c1675e01
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-01-17 17:22:18 -0500

    fix: model not moving on mobile

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -47,7 +47,7 @@ const ProductViewer = () => {
 
             </div>
         </div>
-        <Canvas id='canvas' camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
+        <Canvas id='canvas' style={{ touchAction: 'none' }} camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
             <StudioLights />
             
             <ModelSwitcher scale={isMobile ? scale - 0.03 : scale} isMobile={isMobile} />
